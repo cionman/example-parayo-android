910a83c8d953f74ffbe5ee7a6f09d78f811ffa89
cionman <topspin1278@naver.com>
2020-08-08 19:58:59 +0900

    p.138

---
+++ app/src/main/java/com/example/parayo/signin/SigninActivityUI.kt
@@ -0,0 +1,14 @@
+package com.example.parayo.signin
+
+import android.view.View
+import org.jetbrains.anko.*
+
+class SigninActivityUI(
+    private val viewModel: SigninViewModel
+): AnkoComponent<SigninActivity>{
+    override fun createView(ui: AnkoContext<SigninActivity>): View =
+        ui.linearLayout {
+
+        }
+
+}
